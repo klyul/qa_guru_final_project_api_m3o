85a4fbaed851fa5a1e0d60c5d157363beadd3a03
klyul <yuliya.v3333@gmail.com>
2023-05-17 22:57:24 +0300

    added RestAssured.baseURI,

--- src/test/java/M3OListTablesTest.java
+++ src/test/java/M3OListTablesTest.java
@@ -1,16 +1,16 @@
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 
 public class M3OListTablesTest {
 
     @BeforeAll
     static void beforeAll() {
-
+        RestAssured.baseURI = "https://api.m3o.com";
     }
 
     @Test
@@ -23,7 +23,7 @@ public class M3OListTablesTest {
                         .header("authorization", "Bearer ZDQ3YWQ0NDQtNTJiNC00NzIyLTk5ODYtMDhmYTIwZDFmNWE3")
                         .log().all()
                         .when()
-                        .get("https://api.m3o.com/v1/db/ListTables")
+                        .get("/v1/db/ListTables")
                         .then()
                         .log().all();
 
